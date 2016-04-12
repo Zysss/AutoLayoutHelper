b18a98df55aba3a0a5566b3d06b4bbf0e369c2b7
zhangyansong <zhangyansong@lianjia.com>
2016-04-12 15:06:43 +0800

    great project

---
+++ AutoLayoutHelper/AutoLayoutHelper/AutoLayoutHelper.h
@@ -0,0 +1,19 @@
+//
+//  AutoLayoutHelper.h
+//  AutoLayoutHelper
+//
+//  Created by zysMac on 16/4/12.
+//  Copyright © 2016年 zysMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AutoLayoutHelper.
+FOUNDATION_EXPORT double AutoLayoutHelperVersionNumber;
+
+//! Project version string for AutoLayoutHelper.
+FOUNDATION_EXPORT const unsigned char AutoLayoutHelperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AutoLayoutHelper/PublicHeader.h>
+
+
