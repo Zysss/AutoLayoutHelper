e28c846b97e90ad3f8253c50e82e4b6f698a1dcd
zhangyansong <zhangyansong@lianjia.com>
2016-04-18 10:58:08 +0800

    test

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -13,6 +13,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        let viewRed:UIView = UIView(frame: CGRectZero)
+        let viewBlue:UIView = UIView(frame: CGRectZero)
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
